1e5d44cb45e52b7f1c18b8c1a76e42ec97bb3ede
Ayman Nedjmeddine <theycallmethedr@gmail.com>
2017-03-26 14:09:54 +0100

    update

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const port = 8000
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+app.use('/node_modules', express.static('node_modules'))
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
-});
+
+const history = ['Room Opened']
+io.on('connection', socket => {
+  socket.emit('history', history.join('|'))
+
+  socket.on('chat message', msg => {
+    history.push(msg)
+    io.emit('chat message', msg)
+  })
+})
+
+server.listen(port, () => console.log('listening on *:' + port))
